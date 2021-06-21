ecb152c5c30554efcbbae8d839ddec3370852fc6
Roberto Carrasco <rcarrasco8@miners.utep.edu>
2021-06-21 11:24:21 -0600

    Fixed 8x12 font traversal

--- draw_chars.c
+++ draw_chars.c
@@ -17,11 +17,11 @@ void print_char_11x16(char c)
 void print_char_8x12(char c)
 {
   c -= 0x20;
-  for ( char row = 0; row < 8; row++)
+  for ( char row = 0; row < 11; row++)
     {
       unsigned short rowBits = font_8x12[c][row];
       for(char col = 0; col < 12; col++){
-	unsigned shor colMask = 1 << (11-col);
+	unsigned short colMask = 1 << (11-col);
 	putchar((rowBits & colMask) ? '*':' ');
       }
       putchar('\n');
--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c,Chars 8x12 = d, Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
