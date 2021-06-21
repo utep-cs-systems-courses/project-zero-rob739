fc9280a002fcb75349cd63850c2f44e92f303a16
Roberto Carrasco <rcarrasco8@miners.utep.edu>
2021-06-21 10:22:02 -0600

    Added arrow and chars_8x12 case in menu switch for uimain.c

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -16,6 +16,15 @@ int main()
 
     // Given the user answer, select which method to call
     switch (c) {
+    case 'a':
+      puts("You selected arrow");
+      print_arrow(0,0);
+      break;
+    case 'd':
+      puts("You selected chars 8x12");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
+      break;
     case 't':
       puts("You selected triangle:");
       print_triangle(5, 7);
